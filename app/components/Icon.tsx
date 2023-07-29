9f86694bfd15a8b25747aa79243019bfc3b00757
taiti09 <taitiabe1997@gmail.com>
2023-07-29 18:17:38 +0900

    デプロイエラー対処

---
+++ app/components/Icon.tsx
@@ -0,0 +1,22 @@
+'use client'
+import React from 'react';
+import { AiOutlineTwitter,AiOutlineGithub,AiFillStar } from "react-icons/ai";
+
+
+export const Twitter = () => {
+    return (
+        <AiOutlineTwitter />
+    );
+};
+
+export const GitHub = () => {
+    return (
+        <AiOutlineGithub />
+    );
+};
+
+export const Star = () => {
+    return (
+        <AiFillStar color={'#ffd700'}  />
+    );
+};
