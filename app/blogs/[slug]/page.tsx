cdd7287285c2cdc2ff8d0797d8da88b5cae3f85a
taiti09 <taitiabe1997@gmail.com>
2023-08-18 18:43:05 +0900

    各ページのOGP設定追加

--- app/blogs/[slug]/page.tsx
+++ app/blogs/[slug]/page.tsx
@@ -13,6 +13,41 @@ import 'zenn-content-css';
 import "./contents.css"
 import Article from '@/app/components/Article';
 
+const siteName = 'おーがのプロフィール';
+const description = 'しがないエンジニア　おーがのプロフィールです';
+const url = 'https://t09-blog.com/profile';
+const title = 'Blogs';
+
+export const metadata = {
+    title: {
+      default: siteName,
+      /** `next-seo`の`titleTemplate`に相当する機能 */
+      template: `%s - ${siteName}`,
+    },
+    description,
+    openGraph: {
+      title: siteName,
+      description,
+      url,
+      siteName,
+      locale: 'ja_JP',
+      type: 'website',
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: siteName,
+      description,
+      creator: '@o_ga09',
+    },
+    verification: {
+      google: process.env.NEXT_PUBLIC_VERIFICATION,
+    },
+    alternates: {
+      canonical: url,
+    },
+    metadataBase: new URL(process.env.NEXT_PUBLIC_URL ?? 'http://localhost:3000'),
+};
+
 export default async function BlogDetail({ params, searchParams }:{
     params: { slug: string }
     searchParams: { [key: string]: string | string[] | undefined }
