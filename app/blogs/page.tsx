3f6c73edb1befd340c7f8f32ed399b2c87c78676
taiti09 <taitiabe1997@gmail.com>
2023-08-18 20:25:55 +0900

    動的OGP設定

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -30,6 +30,35 @@ export async function getArticle() {
   );
   return posts
 };
+
+export async function getarticleById(id:number) {
+  // contentディレクトリ内のマークダウンファイル一覧を取得
+  const postsDirectory = path.join(process.cwd(), 'contents');
+  const fileNames = fs.readdirSync(postsDirectory);
+
+  // 各ファイルの中身を取得
+  const posts = await Promise.all(
+    // 各ファイル情報を取得
+    fileNames.map(async (fileName) => {
+      const filePath = path.join(postsDirectory, fileName);
+      const fileContents = fs.readFileSync(filePath, 'utf8');
+      const { data } = matter(fileContents);
+
+      // slugとfrontmatter(title, date, description)を取得
+      return {
+        slug: fileName.replace('.md', ''),
+        frontmatter: data,
+      };
+    })
+  ).then((posts) =>
+    // 最新日付順に並び替え
+    posts.sort((a, b) => new Date(b.frontmatter.date).getTime() - new Date(a.frontmatter.date).getTime())
+  );
+  if(posts.length < id || id < 0) {
+    return;
+  }
+  return posts[id]
+};
 export default async function Blogs() {
   const articles = await getArticle();
   
