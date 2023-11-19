8432cd252a44e7280c38ef9ce89cc7f2b50041fc
taiti09 <taitiabe1997@gmail.com>
2023-11-19 11:53:30 +0900

    #6 横にスクロールする

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import styles from "../styles/styles.module.css";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import MouseTracking from "./components/MouseTracking";
+import HorizontalScroll from "./components/Horizonscroll";
 
 const Home: FC = () => {
   return (
@@ -14,6 +15,7 @@ const Home: FC = () => {
         <Section key={index} index={index} />
       ))}
       <MouseTracking />
+      <HorizontalScroll />
       <Footer />
     </div>
   );
